25739d73c2d6e71baa0570f42f0d1f381b4fa75c
Zuo Xiang <xianglinks@gmail.com>
2017-05-19 10:12:03 +0200

    Add a flash_win script for i3

---
+++ i3/flash_win.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# About: Flashes the active window.
+# Requires transset-df and a composite manager, like xcompmgr.
+
+transset-df -a -m 0
+sleep .1
+transset-df -a -x 1
+sleep .1
+transset-df -a -m 0
+sleep .1
+transset-df -a -x 1
