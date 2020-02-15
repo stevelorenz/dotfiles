12248becf2ae544dec8942d8de63f705623f868d
Zuo Xiang <xianglinks@gmail.com>
2020-02-15 10:57:44 +0100

    Add blurlock for i3wm.

---
+++ i3/.config/i3/blurlock.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# take screenshot
+import -window root /tmp/screenshot.png
+
+# blur it
+convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
+rm /tmp/screenshot.png
+
+# lock the screen
+i3lock -i /tmp/screenshotblur.png
+
+# sleep 1 adds a small delay to prevent possible race conditions with suspend
+sleep 1
+
+exit 0
