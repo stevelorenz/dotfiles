b290624781b28be88085fa843437f2aa7c880f29
Zuo Xiang <zuoxiang@cisco.com>
2024-01-08 10:04:22 +0800

    Update alacritty configs to TOML and Neovim plugins

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -130,6 +130,7 @@ return require("lazy").setup(
 				"hrsh7th/cmp-path",
 				"hrsh7th/cmp-vsnip",
 				"hrsh7th/vim-vsnip",
+				"hrsh7th/vim-vsnip-integ",
 			},
 		},
 
@@ -202,7 +203,7 @@ return require("lazy").setup(
 		-- stylua: ignore
 		keys = {
 			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-  		  	{ "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+  		  	-- { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
   		  	{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
   		  	{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
   		  	{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
